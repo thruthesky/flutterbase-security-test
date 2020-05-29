578babf85c191e73d963dab71eb6c97f373df283
JaeHo Song <thruthesky@gmail.com>
2020-05-29 21:53:19 +0800

    user test

--- spec/user.spec.js
+++ spec/user.spec.js
@@ -1,32 +1,12 @@
 
 const { setup, teardown } = require('./helper');
 describe('User', () => {
-    let db;
-    let usersCol;
+
+
 
     /// 테스트를 할 때, 설정을 한다.
     beforeAll(async () => {
 
-        const mockData = {
-            "users/thruthesky": {
-                uid: "thruthesky"
-            },
-            "users/id1": {
-                uid: "id1"
-            },
-            "users/id2": {
-                uid: "id1"
-            }
-        };
-
-        const mockUser = {
-            uid: "thruthesky"
-        };
-
-
-        db = await setup(mockUser, mockData);
-
-        usersCol = db.collection('users');
     });
 
 
@@ -34,12 +14,62 @@ describe('User', () => {
     /// 테스트가 끝나면 생성된 앱을 모두 제거한다.
     afterAll(async () => {
         await teardown();
-    })
-
-
-    test('should fail', async () => {
-        await expect(usersCol.add({ data: 'something' })).toDeny();
     });
+
+    test('creating without login', async () => {
+        const db = await setup();
+        const usersCol = db.collection('users');
+        await expect(usersCol.add({})).toDeny();
+    });
+
+
+    test('reating with login', async () => {
+        const db = await setup({uid: 'uid'});
+        const usersCol = db.collection('users');
+        await expect(usersCol.add({data: 'something'})).toAllow();
+    });
+
+
+    test('should success on update & get', async () => {
+        const mockData = {
+            "users/thruthesky": {
+                displayName: "thruthesky"
+            },
+        };
+        const mockUser = {
+            uid: "thruthesky"
+        };
+        const db = await setup(mockUser, mockData);
+        usersCol = db.collection('users');
+
+        await expect(usersCol.doc(mockUser.uid).update({ data: 'something', birthday: '123456' })).toAllow();
+
+        const snapshot = await usersCol.doc(mockUser.uid).get();
+
+        const data = snapshot.data();
+        expect(data.birthday).toEqual('123456');
+    });
+
+
+
+    test('should fail on wrong user update', async () => {
+        /// 저장된 도큐먼트 ID 가 `wrong-uid`
+        const mockData = {
+            "users/wrong-uid": {
+                displayName: "wrong-name"
+            },
+        };
+        /// 로그인은 `thruthesky` 로 함.
+        const mockUser = {
+            uid: "thruthesky"
+        };
+        const db = await setup(mockUser, mockData);
+        usersCol = db.collection('users');
+
+        /// 로그인을 `thruthesky`로 했는데, `wrong-uid` 도큐먼트를 수정하려고 함
+        await expect(usersCol.doc('wrong-uid').update({ data: 'something', birthday: '123456' })).toDeny();
+    });
+
 
 
 
