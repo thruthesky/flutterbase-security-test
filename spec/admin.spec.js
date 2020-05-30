bbc59aa02fca06046de678f0fa256aa332266f34
JaeHo Song <thruthesky@gmail.com>
2020-05-30 18:39:07 +0800

    updating admin account test

--- spec/admin.spec.js
+++ spec/admin.spec.js
@@ -15,61 +15,42 @@ describe('Database rules', () => {
         await teardown();
     })
 
-    test('success on reading without login', async () => {
-        const db = await setup();
-        const col = db.collection('categories');
-        await expect(col.limit(10).get()).toAllow();
-    });
 
-    test('fail on creating without login', async () => {
-        const db = await setup();
-        const col = db.collection('categories');
-        await expect(col.add({ id: 'abc' })).toDeny();
-    });
-
-    test('fail on creating with non-admin account.', async () => {
-        const db = await setup({ uid: 'temp-uid' });
-        const col = db.collection('categories');
-        await expect(col.add({ id: 'abc' })).toDeny();
-    });
-
-    test('success on creating a category with admin account.', async () => {
-        const db = await setup({ uid: 'temp-uid', email: 'thruthesky@gmail.com' });
-        const col = db.collection('categories');
-        await expect(col.doc('abc').set({ id: 'abc', title: 'alphabet' })).toAllow();
-    });
-
-    test('failed on updating a category with non-admin account.', async () => {
-        const db = await setup({ uid: 'uid', email: 'non-admin@gmail.com' });
-        const col = db.collection('categories');
-        await expect(col.doc('abc').update({ title: 'updated title' })).toDeny();
-    });
-    test('success on reading a category with non-admin account.', async () => {
-        const db = await setup({ uid: 'temp-uid', email: 'non-admin@gmail.com' });
-        const col = db.collection('categories');
-        await expect(col.doc('abc').get()).toAllow();
-    });
-    test('fail on changing a category id with admin account.', async () => {
-        const db = await setup({ uid: 'temp-uid', email: 'thruthesky@gmail.com' }, {
-            'categories/abc': {
-                id: 'abc',
-                title: 'alphabet',
-            }
-        });
-        const col = db.collection('categories');
-        await expect(col.doc('abc').update({ id: 'change-should-be-denied' })).toDeny();
+    test("fail on editing 'isAdmin' property by a user", async () => {
+        /// 저장된 도큐먼트 ID 가 `a-user-uid`
+        const mockData = {
+            "users/a-user-uid": {
+                displayName: "user-name",
+                isAdmin: false,
+            },
+        };
+        /// 로그인은 `thruthesky` 로 함.
+        const mockUser = {
+            uid: "a-user-uid",
+        };
+        const db = await setup(mockUser, mockData);
+        usersCol = db.collection('users');
+        await expect(usersCol.doc(mockUser.uid).update({ birthday: 731016 })).toAllow();
+        await expect(usersCol.doc(mockUser.uid).update({ isAdmin: true })).toDeny();
     });
 
 
-    test('success on updating with same id but different title with admin account.', async () => {
-        const db = await setup({ uid: 'temp-uid', email: 'thruthesky@gmail.com' }, {
-            'categories/abc': {
-                id: 'abc',
-                title: 'alphabet',
-            }
-        });
-        const col = db.collection('categories');
-        await expect(col.doc('abc').update({ id: 'abc', title:'new title' })).toAllow();
+    test("fail on editing 'isAdmin' property by admin", async () => {
+        /// 저장된 도큐먼트 ID 가 `a-user-uid`
+        const mockData = {
+            "users/a-user-uid": {
+                displayName: "user-name",
+                isAdmin: true,
+            },
+        };
+        /// 로그인은 `thruthesky` 로 함.
+        const mockUser = {
+            uid: "a-user-uid",
+        };
+        const db = await setup(mockUser, mockData);
+        usersCol = db.collection('users');
+        await expect(usersCol.doc(mockUser.uid).update({ birthday: 731016 })).toAllow();
+        await expect(usersCol.doc(mockUser.uid).update({ isAdmin: true })).toAllow();
     });
 
 
